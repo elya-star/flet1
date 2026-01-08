725fc1ea750d53d7189bfffa27d030de1f3c8293
Elvira <ekadirova63@gmail.com>
2026-01-09 00:15:27 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import flet as ft
+import datetime as dt
+
+def main(page: ft.Page):
+    text_hello = ft.Text(value="Hello World!", color=ft.Colors.GREEN_200)
+
+    def on_button_click(_):
+        page.title = "Моё первое приложение"
+        page.theme_mode = ft.ThemeMode.LIGHT
+        name = name_input.value.strip()
+        print(name)
+        if name:
+            text_hello.color = None
+            text_hello.value = f"Hello {name}"
+            name_input.value = ''
+        else:
+            text_hello.value = "Введите корректное имя" 
+            text_hello.color = ft.Colors.RED_200
+        page.update()
+
+
+    eleveted_button = ft.ElevatedButton("SEND", icon=ft.Icons.SEND, on_click=on_button_click)
+    name_input = ft.TextField(label='Введите имя', on_submit=on_button_click)
+
+    page.add(text_hello, name_input, eleveted_button,)
+
+ft.run(main, view=ft.AppView.WEB_BROWSER, port=8550)
+    
+
